808798d90c501f47fff3187e90af779022cf0a95
Hugo-Ferrari <hugoferrari36@gmail.com>
2025-10-22 10:51:59 -0300

    nav lateral(quase pronta)

--- src/app/components/NavLateral.tsx
+++ src/app/components/NavLateral.tsx
@@ -2,20 +2,15 @@ import React from 'react'
 
 function NavLateral() {
     return (
-        <nav>
-            <div>
-                <ul>
-
-                    <li>Inicio</li>
-                    <li>Curso</li>
-                    <li>Chat Geral</li>
-                    <li>Ranking</li>
-                    <li>Hall da Fama</li>
-                    <li>Calendário</li>
-                </ul>
-            </div>
-
-
+        <nav className="w-65 h-screen bg-[#F9F9F9] text-black  flex flex-col justify-between">
+            <ul className="space-y-4 mr-auto ml-auto mt-15 pt-20 ">
+                <li className="hover:bg-[#1561B7]  p-2 rounded-lg cursor-pointer">Início</li> {/*preciso colocar os simbolos a colocar uma referencia para ser acessado */}
+                <li className="hover:bg-[#1561B7]  p-2 rounded-lg cursor-pointer">Curso</li> {/* colocar opacidade no bg */}
+                <li className="hover:bg-[#1561B7]  p-2 rounded-lg cursor-pointer">Chat Geral</li>
+                <li className="hover:bg-[#1561B7]  p-2 rounded-lg cursor-pointer">Ranking</li>
+                <li className="hover:bg-[#1561B7]  p-2 rounded-lg cursor-pointer">Hall da Fama</li>
+                <li className="hover:bg-[#1561B7]  p-2 rounded-lg cursor-pointer">Calendário</li>
+            </ul>
         </nav>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Nav from "./components/Nav";
 import NavLateral from "./components/NavLateral";
 
+
 export default function Home() {
   return (
     <div>
